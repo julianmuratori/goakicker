98fa145a768c13a2a7abf318ec7969184a9328fb
Julian Muratori <julianmuratori@gmail.com>
2018-10-05 16:03:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider>
+        <h3>hl</h3>
+      </MuiThemeProvider>
     );
   }
 }
